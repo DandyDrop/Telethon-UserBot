1abb3f5437ccc1dd6006e6526478bc3154a5e8af
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-01-27 19:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import telethon
+import os
+import time
 
-
+api_id = os.environ.get("API_ID")
+api_hash = os.environ.get("API_HASH")
+client = telethon.TelegramClient(os.environ.get("SNAME"), api_id, api_hash)
+client.start()
+client.run_until_disconnected()
