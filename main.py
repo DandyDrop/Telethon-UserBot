f1bf6be54a88e788a6186d9e0a00887df71cdee0
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-06 19:04:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-from telethon.client import TelegramClient
-from telethon.sessions import StringSession
+import requests
 import os
 import flask
 from flask import Flask, request, Response
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def handle_request():
-    if request.headers.get('content-type') == "application/json":
-        return ""
-    else:
-        flask.abort(403)
-    if request.method == "POST":
-        return Response("OK", status=200)
-    else:
-        return ""
-                
+data = {"pass": os.environ.get('PASS')}
 
-with TelegramClient(StringSession(os.environ.get("STRING_SESSION")), int(os.environ.get("API_ID")), os.environ.get("API_HASH")) as client:
-    client.loop.run_until_complete(client.send_message('me', 'Hello, MAZAFUCKA'))
+@app.route('/', methods=['HEAD'])
+def handle_request():
+    requests.post("https://send.adaptable.app", data=data)
+    return ""
+    
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
 
