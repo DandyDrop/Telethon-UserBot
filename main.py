fdc7e5ff0909a3c057862591177380c8b3c14e3b
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-08 17:00:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+import requests
+import os
+from flask import Flask, request, Response
+app = Flask(__name__)
+
+data = {"a": "as"}
+
+@app.route('/', methods=['HEAD'])
+def handle_request():
+    requests.post(os.environ.get('LINK'), data=data)
+    return ""
+    
+
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+
+
